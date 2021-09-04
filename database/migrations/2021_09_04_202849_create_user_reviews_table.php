2fbf39e9a0431205235c54f6a9858e14f66ee158
Nikola Gjorgjievski <nikola.gjorgjievski@outlook.com>
2021-09-04 22:30:39 +0200

    :card_file_box: Add user reviews table

---
+++ database/migrations/2021_09_04_202849_create_user_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_reviews', function (Blueprint $table) {
+            $table->integerIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('item_id');
+            $table->unsignedInteger('rank');
+            $table->text('comment');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_reviews');
+    }
+}
