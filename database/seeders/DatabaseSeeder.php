45b4d91585fe877fea7869d614b0576219ebd7ce
Nikola Gjorgjievski <nikola.gjorgjievski@outlook.com>
2021-09-04 23:33:14 +0200

    :monocle_face: Add items reviews seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\Genre;
 use App\Models\Item;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,47 +26,78 @@ class DatabaseSeeder extends Seeder
         $movieCategory = Category::create(['name' => 'movie']);
         $bookCategory = Category::create(['name' => 'book']);
 
-        Item::create([
+        $starWars = Item::create([
             'title' => 'Star Wars',
             'isApproved' => true,
             'user_id' => $demoUser->id,
             'category_id' => $movieCategory->id,
             'genre_id' => $sfGenre->id,
         ]);
-        Item::create([
+        $scaryMovie = Item::create([
             'title' => 'Scary Movie',
             'isApproved' => true,
             'user_id' => $demoUser->id,
             'category_id' => $movieCategory->id,
             'genre_id' => $comedyGenre->id,
         ]);
-        Item::create([
+        $fps = Item::create([
             'title' => 'fps',
             'isApproved' => true,
             'user_id' => $demoUser->id,
             'category_id' => $movieCategory->id,
             'genre_id' => $fpsGenre->id,
         ]);
-        Item::create([
+        $mySecretAgent = Item::create([
             'title' => 'My secret agent',
             'isApproved' => true,
             'user_id' => $demoUser->id,
             'category_id' => $movieCategory->id,
             'genre_id' => $comedyGenre->id,
         ]);
-        Item::create([
+        $myComedyBook = Item::create([
             'title' => 'My comedy book',
             'isApproved' => true,
             'user_id' => $demoUser->id,
             'category_id' => $bookCategory->id,
             'genre_id' => $comedyGenre->id,
         ]);
-        Item::create([
+        $battleInTheStars = Item::create([
             'title' => 'Battle in the stars',
             'isApproved' => true,
             'user_id' => $demoAdmin->id,
             'category_id' => $bookCategory->id,
             'genre_id' => $sfGenre->id,
+        ]);
+
+        DB::table('user_reviews')->insert([
+            [
+                'id' => 1,
+                'user_id' => $demoUser->id,
+                'item_id' => $starWars->id,
+                'rank' => 1,
+                'comment' => 'boring',
+            ],
+            [
+                'id' => 2,
+                'user_id' => $demoAdmin->id,
+                'item_id' => $scaryMovie->id,
+                'rank' => 5,
+                'comment' => 'good',
+            ],
+            [
+                'id' => 3,
+                'user_id' => $demoUser->id,
+                'item_id' => $scaryMovie->id,
+                'rank' => 3,
+                'comment' => 'average',
+            ],
+            [
+                'id' => 4,
+                'user_id' => $demoUser->id,
+                'item_id' => $fps->id,
+                'rank' => 10,
+                'comment' => 'brilliant',
+            ],
         ]);
 
     }
