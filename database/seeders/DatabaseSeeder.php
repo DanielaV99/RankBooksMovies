3d01b58b1ccc25ae2131a10e369d786fb8dc07fd
Nikola Gjorgjievski <nikola.gjorgjievski@outlook.com>
2021-09-04 23:04:39 +0200

    :monocle_face: Add items seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Genre;
+use App\Models\Item;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +17,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()->demoUser()->create();
-        \App\Models\User::factory()->demoAdmin()->create();
+        $demoUser = User::factory()->demoUser()->create();
+        $demoAdmin = User::factory()->demoAdmin()->create();
+        $sfGenre = Genre::create(['name' => 'SF']);
+        $comedyGenre = Genre::create(['name' => 'comedy']);
+        $fpsGenre = Genre::create(['name' => 'FPS']);
+        $movieCategory = Category::create(['name' => 'movie']);
+        $bookCategory = Category::create(['name' => 'book']);
+
+        Item::create([
+            'title' => 'Star Wars',
+            'isApproved' => true,
+            'user_id' => $demoUser->id,
+            'category_id' => $movieCategory->id,
+            'genre_id' => $sfGenre->id,
+        ]);
+        Item::create([
+            'title' => 'Scary Movie',
+            'isApproved' => true,
+            'user_id' => $demoUser->id,
+            'category_id' => $movieCategory->id,
+            'genre_id' => $comedyGenre->id,
+        ]);
+        Item::create([
+            'title' => 'fps',
+            'isApproved' => true,
+            'user_id' => $demoUser->id,
+            'category_id' => $movieCategory->id,
+            'genre_id' => $fpsGenre->id,
+        ]);
+        Item::create([
+            'title' => 'My secret agent',
+            'isApproved' => true,
+            'user_id' => $demoUser->id,
+            'category_id' => $movieCategory->id,
+            'genre_id' => $comedyGenre->id,
+        ]);
+        Item::create([
+            'title' => 'My comedy book',
+            'isApproved' => true,
+            'user_id' => $demoUser->id,
+            'category_id' => $bookCategory->id,
+            'genre_id' => $comedyGenre->id,
+        ]);
+        Item::create([
+            'title' => 'Battle in the stars',
+            'isApproved' => true,
+            'user_id' => $demoAdmin->id,
+            'category_id' => $bookCategory->id,
+            'genre_id' => $sfGenre->id,
+        ]);
+
     }
 }
