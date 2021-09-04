92aad2ef1495087a1eceafbc40b5b6b4c41d7443
Nikola Gjorgjievski <nikola.gjorgjievski@outlook.com>
2021-09-04 23:34:04 +0200

    :construction: Add rank items controller

--- resources/views/rank-items.blade.php
+++ resources/views/rank-items.blade.php
@@ -8,9 +8,27 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+                @foreach ($items as $item)
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <p>
+                            {{ $item->title }}
+                            {{ $item->category->name }}
+                            {{ $item->genre->name }}
+                            {{ $item->createdBy->name }}
+                            @if (count($item->userReviews))
+                                @php
+                                    $sumRanks = null;
+                                @endphp
+                                @foreach ($item->userReviews as $userReview)
+                                    @php
+                                        $sumRanks += $userReview->pivot->rank;
+                                    @endphp
+                                @endforeach
+                                {{ $sumRanks/count($item->userReviews) }}
+                            @endif
+                        </p>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/rank-items', function () {
-    return view('rank-items');
-})->middleware(['auth'])->name('rank-items');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/rank-items', [ItemController::class, 'rankItems'])->name('rank-items');
+});
 
 require __DIR__.'/auth.php';
