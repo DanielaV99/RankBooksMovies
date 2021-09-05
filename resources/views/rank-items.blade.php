5e697cdae3ed2aabde202dd1a1281f7b60cc611a
Nikola Gjorgjievski <nikola.gjorgjievski@outlook.com>
2021-09-05 13:04:39 +0200

    Small UI improvements

--- resources/views/rank-items.blade.php
+++ resources/views/rank-items.blade.php
@@ -35,32 +35,32 @@
                             </div>
 
                             <div class="flex flex-grow items-center justify-end flex-wrap">
-                                <div class="flex flex-grow items-center justify-end">
-                                    @php
-                                        $sumRanks = null;
-                                        $hasReviewed = false;
-                                    @endphp
+                                @php
+                                    $sumRanks = null;
+                                    $hasReviewed = false;
+                                    foreach ($item->userReviews as $userReview) {
+                                        $sumRanks += $userReview->pivot->rank;
+                                        if ($userReview->pivot->user_id === $currentUserId) {
+                                            $hasReviewed = true;
+                                        }
+                                    }
+                                @endphp
+                                <div class="flex flex-grow items-center justify-end flex-wrap">
                                     @if (count($item->userReviews))
-                                        @foreach ($item->userReviews as $userReview)
-                                            @php
-                                                $sumRanks += $userReview->pivot->rank;
-                                                if ($userReview->pivot->user_id === $currentUserId) {
-                                                    $hasReviewed = true;
-                                                }
-                                            @endphp
-                                        @endforeach
-                                        <x-rank :rank="$sumRanks/count($item->userReviews)"></x-rank>
-                                    @endif
-                                    @if (!$hasReviewed)
-                                        <a href="{{ route('item.review.create', $item->id) }}" class="ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-                                            {{ __('Add Review') }}
-                                        </a>
+                                        <div class="w-full flex flex-grow items-center justify-end">
+                                            <x-rank :rank="$sumRanks/count($item->userReviews)"></x-rank>
+                                        </div>
                                     @endif
                                 </div>
                                 @if (count($item->userReviews))
-                                    <p class="w-full text-xs font-thin flex flex-grow items-center justify-end">
+                                    <p class="mb-4 w-full text-xs font-thin flex flex-grow items-center justify-end">
                                         {{ 'Reviews: ' . count($item->userReviews) }}
                                     </p>
+                                @endif
+                                @if (!$hasReviewed)
+                                    <a href="{{ route('item.review.create', $item->id) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                        {{ __('Add Review') }}
+                                    </a>
                                 @endif
                             </div>
                         </div>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -51,6 +51,14 @@
             </div>
         </div>
 
+        @if (!$hasReviewed)
+            <div class="mt-6 w-full max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <a href="{{ route('item.review.create', $item->id) }}" class="ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                    {{ __('Add Review') }}
+                </a>
+            </div>
+        @endif
+
         @foreach($item->userReviews as $userReview)
             <div class="mt-6 w-full max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
