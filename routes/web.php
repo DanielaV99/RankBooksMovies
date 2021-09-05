50dbf9d149133ab97c681ca316ec02eb1cbdc6b4
Nikola Gjorgjievski <nikola.gjorgjievski@outlook.com>
2021-09-05 16:01:44 +0200

    Fix auth layout

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('rank-items'));
 });
 
+Route::get('/rank-items', [ItemController::class, 'rankItems'])->name('rank-items');
+Route::get('/show/{id}', [ItemController::class, 'show'])->name('item.show');
+
 Route::middleware(['auth'])->group(function () {
-    Route::get('/rank-items', [ItemController::class, 'rankItems'])->name('rank-items');
     Route::get('/create', [ItemController::class, 'create'])->name('item.create');
     Route::get('/create-approve', [ItemController::class, 'createApprove'])->name('item.create.approve');
     Route::post('/create-approve-store', [ItemController::class, 'createApproveStore'])->name('item.create.approve.store');
     Route::post('/store', [ItemController::class, 'store'])->name('item.store');
     Route::get('/store-success', [ItemController::class, 'storeSuccess'])->name('item.store.success');
-    Route::get('/show/{id}', [ItemController::class, 'show'])->name('item.show');
     Route::get('/add-review/{id}', [ItemController::class, 'reviewCreate'])->name('item.review.create');
     Route::post('/review-store', [ItemController::class, 'reviewStore'])->name('item.review.store');
 });
